9ee674297ac0927f468a3a6572394554cb7e36fa
Cyrus Yip <cyruscmyip1@gmail.com>
2021-03-29 11:24:05 -0700

    events! Last message event. (b.last)

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 class BronzeMedalist(commands.Bot):
     def __init__(self, *args, **kwargs):
-        super().__init__(command_prefix="/", activity=discord.Game("bronze medals op"), *args, **kwargs)
+        super().__init__(command_prefix="b.", activity=discord.Game("bronze medals op"), intents=discord.Intents.all(), *args, **kwargs)
         self.remove_command("help")
         self.loading_cogs = ["cogs.currency", "cogs.misc"]
         deta_key = os.environ.get("DETA_KEY", None)
@@ -22,6 +22,11 @@ class BronzeMedalist(commands.Bot):
             raise RuntimeError
         deta = Deta(deta_key)
         self.db = deta.Base(os.environ.get("DETABASE_NAME", "BronzeMedalist"))
+        self.event_starters = list(
+            map(int, str(os.environ.get("EVENT_STARTER_IDS", None)).split(","))) if os.environ.get(
+            "EVENT_STARTER_IDS", None) else None
+        # Last message event data
+        self.last_event = {"in_progress": False, "users": [], "channel": 0}
         self.startup()
 
     async def on_ready(self):
@@ -49,9 +54,17 @@ class BronzeMedalist(commands.Bot):
             except Exception as e:
                 print(f"Failed to load {cog}. Error type: {type(e).__name__}")
 
+    async def on_message(self, message):
+        if message.channel.id == self.last_event["channel"] and not message.author.bot:
+            if message.author.id in self.last_event["users"]:
+                await message.delete()
+            else:
+                self.last_event["users"].append(message.author.id)
+        await self.process_commands(message)
+
 
 bot = BronzeMedalist()
-slash = SlashCommand(bot, override_type=True, sync_commands=True)
+slash = SlashCommand(bot, sync_commands=True)
 bot.load_cogs()
 
 token = os.environ.get("TOKEN", None)
